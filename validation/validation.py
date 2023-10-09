9bc86e7a48e3916953b69eee2156ee461046fee8
unknown <martinrolo22@estudiantec.cr>
2023-10-09 16:29:52 -0600

    update: UI new version

--- validation/validation.py
+++ validation/validation.py
@@ -8,34 +8,34 @@ def processing(path, typeDoc):
    
     image = ImageEnhancer(path)
     image.process(typeDoc)
-    image.display_image()
+    #image.display_image()
     
-    original_image = ImageEnhancer(path)
-    original_image.load_image()
-    original_image.resize()
-    original_image.display_concatenated_image(image.image)
+    #original_image = ImageEnhancer(path)
+    #original_image.load_image()
+    #original_image.resize()
+    #original_image.display_concatenated_image(image.image)
     return path_enhanced_image
 
 
-def validate(path):
+def validate(path_enhanced_image, typeDoc):
     value = False
-    typeDoc = categorize_local(path)
-    path_enhanced_image = processing(path, typeDoc)
+    
     if typeDoc  == "cedula":
         data = get_text_img_identity(path_enhanced_image)
         value = identitycard_compare(data)
-
-
+        return data, value
     elif typeDoc == "carnet":
         data = get_text_img_stundent(path_enhanced_image)
         value = studentcard_compare(data)
+        return data, value
     elif typeDoc == "licencia":
         data = get_text_img_license(path_enhanced_image)
         value = license_compare(data)
+        return data, value
        
     else:
         print("El documento no es del tipo adecuado")
-
+        return None, None
     if value == True:
         print("Documento Valido")
     else:
